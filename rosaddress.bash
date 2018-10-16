c979bbe88e35d7048915fe5e7ddd63723335d9a2
RyodoTanaka <groadpg@gmail.com>
2018-10-16 19:37:08 +0900

    [update] delete wlan0

--- rosaddress.bash
+++ rosaddress.bash
@@ -1,24 +1,5 @@
 #!/bin/bash
 function _func_rosserver() {
-	if [ -z "$eth0_addr" ]; then
-		echo "No eth0 connection..."
-		if [ -z  "$wlan0_addr" ]; then
-			echo "No wlan0 connection..."
-			export ROS_MASTER_URI=http://localhost:11311
-			unset ROS_HOST_NAME
-			unset ROS_IP
-			echo ""
-			echo "            !!CAUTION!! "
-			echo "  There're no Ethernet connection."
-			echo "  We CAN NOT set this PC to ROS server."
-			echo ""
-		else
-			echo "There are wlan0 connection"
-			export ROS_MASTER_URI=http://${wlan0_addr}:11311
-			export ROS_HOST_NAME=${wlan0_addr}
-			export ROS_IP=${wlan0_addr}
-			export PS1="\[\033[41;1;33m\]<ROS_server>\[\033[0m\]\w$ "
-		fi	
 	else
 		echo "There are eth0 connection"
 		export ROS_MASTER_URI=http://${eth0_addr}:11311
@@ -37,25 +18,6 @@ function _func_rosclient() {
 		echo $1
 		echo "Input the ROS server's IP address.'"
 	else
-		if [ -z "$eth0_addr" ]; then
-			echo "No eth0 connection..."
-			if [ -z  "$wlan0_addr" ]; then
-				echo "No wlan0 connection..."
-				export ROS_MASTER_URI=http://localhost:11311
-				unset ROS_HOST_NAME
-				unset ROS_IP
-				echo ""
-				echo "            !!CAUTION!! "
-				echo "  There're no Ethernet connection."
-				echo "  We CAN NOT set this PC to ROS client."
-				echo ""
-			else
-				echo "There are wlan0 connection"
-				export ROS_MASTER_URI=http://$1:11311
-				export ROS_HOST_NAME=${wlan0_addr}
-				export ROS_IP=${wlan0_addr}
-				export PS1="\[\033[44;1;33m\]<ROS_client>\[\033[0m\]\w$ "
-			fi	
 		else
 			echo "There are eth0 connection"
 			export ROS_MASTER_URI=http://$1:11311
@@ -97,7 +59,6 @@ function _func_rosaddress() {
 	# Get now eth0 or wlan0 IP address
     eth0_name=(`ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}'`)
 	eth0_addr=$(ip -f inet -o addr show ${eth0_name[0]}|cut -d\  -f 7 | cut -d/ -f 1)
-	wlan0_addr=$(ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1)
 
 	if [ $1 = "local" ]; then
 		_func_roslocal
